073e45fdfb4a23d36bdf6a58c8f882bcaf3d93f2
TheJeff01 <jeffreyibeh501@gmail.com>
2025-05-03 21:53:32 +0100

    added an underline and added a container

--- src/components/ProblemSolver.jsx
+++ src/components/ProblemSolver.jsx
@@ -16,68 +16,63 @@ const charVariants = {
 const data = [
   {
     boxicon: <MdCheckBoxOutlineBlank />,
-    text:
-      "Holder of a Bachelor’s Degree in Computer Science from Coal City University, Enugu State.",
+    text: "Holder of a Bachelor’s Degree in Computer Science from Coal City University, Enugu State.",
   },
   {
     boxicon: <MdCheckBoxOutlineBlank />,
-    text:
-      "Skilled in React.js and Next.js, with a passion for staying ahead of the curve through continuous learning and adoption of emerging technologies.",
+    text: "Skilled in React.js and Next.js, with a passion for staying ahead of the curve through continuous learning and adoption of emerging technologies.",
   },
   {
     boxicon: <MdCheckBoxOutlineBlank />,
-    text:
-      "I love working in a team environment, but I can also work independently.",
+    text: "I love working in a team environment, but I can also work independently.",
   },
 ];
 
 function ProblemSolver() {
   return (
     <main className="container max-w-7xl mx-auto w-full px-4">
-      <div className="text-center mb-[34px] lg:mb-[80px]">
-        <motion.h1
-          initial="hidden"
-          whileInView="reveal"
-          transition={{ staggerChildren: 0.02 }}
-          className="text-[48px] mb-[12px] lg:text-[70px] font-bold"
-        >
-          {heading.split("").map((char, index) => (
-            <motion.span
-              key={index}
-              transition={{ duration: 0.5 }}
-              variants={charVariants}
-            >
-              {char}
-            </motion.span>
+      <div className="border-b-[2px]">
+        <div className="text-center mb-[34px] lg:mb-[80px] ">
+          <motion.h1
+            initial="hidden"
+            whileInView="reveal"
+            transition={{ staggerChildren: 0.02 }}
+            className="text-[48px] mb-[12px] lg:text-[70px] font-bold"
+          >
+            {heading.split("").map((char, index) => (
+              <motion.span
+                key={index}
+                transition={{ duration: 0.5 }}
+                variants={charVariants}
+              >
+                {char}
+              </motion.span>
+            ))}
+          </motion.h1>
+          <motion.p
+            initial="hidden"
+            whileInView="reveal"
+            transition={{ staggerChildren: 0.02 }}
+            className="mb-[12px] text-[20px] lg:text-[24px]"
+          >
+            {subheading.split("").map((char, index) => (
+              <motion.span
+                key={index}
+                transition={{ duration: 0.35 }}
+                variants={charVariants}
+              >
+                {char}
+              </motion.span>
+            ))}
+          </motion.p>
+        </div>
+        <div>
+          {data.map((option, index) => (
+            <React.Fragment key={index}>
+              <ProblemSolverBrief boxicon={option.boxicon} text={option.text} />
+            </React.Fragment>
           ))}
-        </motion.h1>
-        <motion.p
-          initial="hidden"
-          whileInView="reveal"
-          transition={{ staggerChildren: 0.02 }}
-          className="mb-[12px] text-[20px] lg:text-[24px]"
-        >
-          {subheading.split("").map((char, index) => (
-            <motion.span
-              key={index}
-              transition={{ duration: 0.35 }}
-              variants={charVariants}
-            >
-              {char}
-            </motion.span>
-          ))}
-        </motion.p>
-      </div>
-      <div>
-        {data.map((option, index) => (
-          <React.Fragment key={index}>
-            <ProblemSolverBrief
-              boxicon={option.boxicon}
-              text={option.text}
-            />
-          </React.Fragment>
-        ))}
-        
+        </div>
       </div>
     </main>
   );
--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -55,7 +55,7 @@ const techStack = [
 
 function TechStack() {
   return (
-    <main className="">
+    <main className="container">
       {techStack.map((option, index) => (
         <React.Fragment key={index}>
           <TechStackCard
