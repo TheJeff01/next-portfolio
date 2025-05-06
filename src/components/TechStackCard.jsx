9137750a54c919fde951269520cc0da7f2f2a34f
TheJeff01 <jeffreyibeh501@gmail.com>
2025-05-06 15:21:31 +0100

    i deleted the hood image

--- src/components/TechStackCard.jsx
+++ src/components/TechStackCard.jsx
@@ -1,9 +1,33 @@
+"use client";
+import splitStringUsingRegex from "./utils/splitStringUsingRegex";
+import { motion } from "framer-motion";
+import React from "react";
+
+const charVariants = {
+  hidden: { opacity: 0 },
+  reveal: { opacity: 1 },
+};
+
 function TechStackCard(props) {
   return (
     <div className="p-[10px] text-[20px]">
       <div className="text-[40px] mb-[10px]">{props.icon}</div>
       <div className="font-bold mb-[10px]">{props.iconName}</div>
-      <div>{props.text}</div>
+      <motion.p
+              initial="hidden"
+              whileInView="reveal"
+              transition={{ staggerChildren: 0.02 }}
+            >
+              {props.text.split("").map((char, index) => (
+                <motion.span
+                  key={index}
+                  transition={{ duration: 0.5 }}
+                  variants={charVariants}
+                >
+                  {char}
+                </motion.span>
+              ))}
+            </motion.p>
     </div>
   );
 }
--- src/components/assets/images/Images.jsx
+++ src/components/assets/images/Images.jsx
@@ -2,6 +2,5 @@ export const Images = {
   logo: require("./svgs/logo.svg").default.src,
   contact: require("./svgs/contact.svg").default.src,
   menu: require("./svgs/menu.svg").default.src,
-  hoodImage: require("./Hood-image.png").default.src,
   jeff: require("./Jeff.png").default.src
 };
